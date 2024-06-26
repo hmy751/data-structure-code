a04ef8a46f600b1643858fa3cfbb40e2a299fb61
hmy751 <hmy751@gmail.com>
2024-06-26 20:06:22 +0900

    chore: doubly linked list, 코드 describe로 중첩 수정

--- src/dataStructure/doublyLinkedList.spec.ts
+++ src/dataStructure/doublyLinkedList.spec.ts
@@ -1,80 +1,82 @@
 import { DoublyLinkedList } from "./doublyLinkedList";
 
-it("리스트 생성", () => {
-  const list = new DoublyLinkedList();
+describe("Test Doubly Linked List", () => {
+  it("리스트 생성", () => {
+    const list = new DoublyLinkedList();
 
-  expect(list.head).toBe(null);
-  expect(list.tail).toBe(null);
-});
+    expect(list.head).toBe(null);
+    expect(list.tail).toBe(null);
+  });
 
-it("리스트 생성 후 노드 추가", () => {
-  const list = new DoublyLinkedList();
+  it("리스트 생성 후 노드 추가", () => {
+    const list = new DoublyLinkedList();
 
-  list.push(1);
+    list.push(1);
 
-  expect(list.head?.value).toBe(1);
-  expect(list.tail?.value).toBe(1);
-  expect(list.size).toBe(1);
-});
+    expect(list.head?.value).toBe(1);
+    expect(list.tail?.value).toBe(1);
+    expect(list.size).toBe(1);
+  });
 
-it("기존 리스트에 노드 추가", () => {
-  const list = new DoublyLinkedList();
+  it("기존 리스트에 노드 추가", () => {
+    const list = new DoublyLinkedList();
 
-  list.push(1);
-  list.push(2);
+    list.push(1);
+    list.push(2);
 
-  expect(list.head?.value).toBe(1);
-  expect(list.tail?.value).toBe(2);
-  expect(list.head?.next).toBe(list.tail);
-  expect(list.tail?.prev).toBe(list.head);
-  expect(list.size).toBe(2);
-});
+    expect(list.head?.value).toBe(1);
+    expect(list.tail?.value).toBe(2);
+    expect(list.head?.next).toBe(list.tail);
+    expect(list.tail?.prev).toBe(list.head);
+    expect(list.size).toBe(2);
+  });
 
-it("기존 리스트에서 노드 제거", () => {
-  const list = new DoublyLinkedList();
+  it("기존 리스트에서 노드 제거", () => {
+    const list = new DoublyLinkedList();
 
-  list.push(1);
-  list.push(2);
-  list.push(3);
-  list.push(4);
+    list.push(1);
+    list.push(2);
+    list.push(3);
+    list.push(4);
 
-  const firstPopNode = list.pop();
-  expect(firstPopNode?.value).toBe(4);
+    const firstPopNode = list.pop();
+    expect(firstPopNode?.value).toBe(4);
 
-  const secondPopNode = list.pop();
-  expect(secondPopNode?.value).toBe(3);
+    const secondPopNode = list.pop();
+    expect(secondPopNode?.value).toBe(3);
 
-  expect(list?.tail?.value).toBe(2);
-  expect(list?.tail?.prev?.value).toBe(1);
-  expect(list.size).toBe(2);
-});
+    expect(list?.tail?.value).toBe(2);
+    expect(list?.tail?.prev?.value).toBe(1);
+    expect(list.size).toBe(2);
+  });
 
-it("Head가 1개일 때 노드 제거", () => {
-  const list = new DoublyLinkedList();
+  it("Head가 1개일 때 노드 제거", () => {
+    const list = new DoublyLinkedList();
 
-  list.push(1);
-  const popNode = list.pop();
+    list.push(1);
+    const popNode = list.pop();
 
-  expect(popNode?.value).toBe(1);
-  expect(list.head).toBe(null);
-  expect(list.tail).toBe(null);
-  expect(list.size).toBe(0);
-});
+    expect(popNode?.value).toBe(1);
+    expect(list.head).toBe(null);
+    expect(list.tail).toBe(null);
+    expect(list.size).toBe(0);
+  });
 
-it("리스트의 노드가 없을 때 노드 제거", () => {
-  const list = new DoublyLinkedList();
+  it("리스트의 노드가 없을 때 노드 제거", () => {
+    const list = new DoublyLinkedList();
 
-  list.push(1);
-  list.pop();
+    list.push(1);
+    list.pop();
 
-  expect(list.head).toBe(null);
-  expect(list.tail).toBe(null);
-  expect(list.size).toBe(0);
+    expect(list.head).toBe(null);
+    expect(list.tail).toBe(null);
+    expect(list.size).toBe(0);
 
-  const popNode = list.pop();
+    const popNode = list.pop();
 
-  expect(popNode).toBe(null);
-  expect(list.head).toBe(null);
-  expect(list.tail).toBe(null);
-  expect(list.size).toBe(0);
+    expect(popNode).toBe(null);
+    expect(list.head).toBe(null);
+    expect(list.tail).toBe(null);
+    expect(list.size).toBe(0);
+  });
 });
