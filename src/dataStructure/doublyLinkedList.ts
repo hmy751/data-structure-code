4773ba3428a5b6c267c47f87f4c4ec537b4c1122
hmy751 <hmy751@gmail.com>
2024-06-26 11:24:39 +0900

    feat: doubly linked list 기본 구현

---
+++ src/dataStructure/doublyLinkedList.ts
@@ -0,0 +1,24 @@
+type ValueType = string | number | Record<string, unknown> | Array<unknown>;
+
+export class DoublyLinkedListNode {
+  readonly value: ValueType;
+  readonly prev: DoublyLinkedListNode | null;
+  readonly next: DoublyLinkedListNode | null;
+
+  constructor(value: ValueType) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+export class DoublyLinkedList {
+  readonly head: DoublyLinkedListNode | null;
+  readonly tail: DoublyLinkedListNode | null;
+  readonly _size: number = 0;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
