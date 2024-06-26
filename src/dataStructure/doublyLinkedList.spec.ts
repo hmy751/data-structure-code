9fd4c5be9ac09f7c44b1a6af2701cd46dccb8cbc
hmy751 <hmy751@gmail.com>
2024-06-26 11:25:01 +0900

    test: doubly linked list 기본 리스트 생성 테스트 작성

---
+++ src/dataStructure/doublyLinkedList.spec.ts
@@ -0,0 +1,63 @@
+import { DoublyLinkedList } from "./doublyLinkedList";
+
+it("리스트 생성", () => {
+  const list = new DoublyLinkedList();
+
+  expect(list.head).toBe(null);
+  expect(list.tail).toBe(null);
+});
+
+it("리스트 생성 후 노드 추가", () => {
+  // const list = new DoublyLinkedList();
+  // list.push(1);
+  // expect(list.head?.value).toBe(1);
+  // expect(list.tail?.value).toBe(1);
+  // expect(list.size).toBe(1);
+});
+
+it("기존 리스트에 노드 추가", () => {
+  // const list = new DoublyLinkedList();
+  // list.push(1);
+  // list.push(2);
+  // // prev, next 점검
+  // expect(list.head?.value).toBe(1);
+  // expect(list.tail?.value).toBe(2);
+  // expect(list.head?.next?.value).toBe(2);
+  // expect(list.tail?.prev?.value).toBe(1);
+  // expect(list.size).toBe(2);
+});
+
+it("기존 리스트에서 노드 제거", () => {
+  // const list = new DoublyLinkedList();
+  // list.push(1);
+  // list.push(2);
+  // list.push(3);
+  // list.push(4);
+  // list.pop();
+  // list.pop();
+  // expect(list?.tail?.value).toBe(2);
+  // expect(list?.tail?.prev?.value).toBe(1);
+  // expect(list.size).toBe(2);
+});
+
+it("Head가 1개일 때 노드 제거", () => {
+  // const list = new DoublyLinkedList();
+  // list.push(1);
+  // list.pop();
+  // expect(list.head).toBe(null);
+  // expect(list.tail).toBe(null);
+  // expect(list.size).toBe(0);
+});
+
+it("리스트의 노드가 없을 때 노드 제거", () => {
+  // const list = new DoublyLinkedList();
+  // list.push(1);
+  // list.pop();
+  // expect(list.head).toBe(null);
+  // expect(list.tail).toBe(null);
+  // expect(list.size).toBe(0);
+  // expect(list.pop()).toBe(null);
+  // expect(list.head).toBe(null);
+  // expect(list.tail).toBe(null);
+  // expect(list.size).toBe(0);
+});
