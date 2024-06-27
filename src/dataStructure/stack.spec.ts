3686789f46c21d7f614c7eb40ad920ad87cb4e94
hmy751 <hmy751@gmail.com>
2024-06-27 16:56:13 +0900

    test: stack, 가장 나중에 넣은 데이터가 먼저 나온다, 테스트 작성

--- src/dataStructure/stack.spec.ts
+++ src/dataStructure/stack.spec.ts
@@ -1,1 +1,17 @@
-describe("Test Stack", () => {});
+import { Stack } from "./stack";
+
+describe("Test Stack", () => {
+  it("가장 나중에 넣은 데이터가 먼저 나온다.", () => {
+    const stack = new Stack();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    const firstNode = stack.pop();
+    expect(firstNode.value).toBe(3);
+
+    const secondNode = stack.pop();
+    expect(secondNode.value).toBe(2);
+  });
+});
