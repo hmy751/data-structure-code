3735387b119224548ce784c29b8dbc570afb7eea
hmy751 <hmy751@gmail.com>
2024-06-27 17:23:00 +0900

    feat: stack, linked list로 구현

--- src/dataStructure/stack.ts
+++ src/dataStructure/stack.ts
@@ -1,1 +1,23 @@
+type ValueType = string | number | Record<string, unknown> | Array<unknown>;
 import { SinglyLinkedList } from "./singlyLinkedList";
+
+export class Stack {
+  list: SinglyLinkedList;
+
+  constructor() {
+    this.list = new SinglyLinkedList();
+  }
+
+  push(value: ValueType) {
+    this.list.push(value);
+  }
+
+  pop(): ValueType | null {
+    const current = this.list.pop();
+    return current?.value || null;
+  }
+
+  get size(): number {
+    return this.list.size;
+  }
+}
