faa8223c70783e7090c23fa295b13d3bc73bde7f
hmy751 <hmy751@gmail.com>
2024-06-28 17:20:39 +0900

    test: binary tree, inorderTraverse 테스트, 작성

--- src/dataStructure/binaryTree.spec.ts
+++ src/dataStructure/binaryTree.spec.ts
@@ -28,4 +28,32 @@ describe("Test Binary test", () => {
       JSON.stringify(["A", "B", "D", "E", "F", "C"])
     );
   });
+
+  it("inorder(중위순회) 테스트", () => {
+    const tree = new BinaryTree();
+
+    tree.root = new Node("A");
+
+    tree.root.left = new Node("B");
+    tree.root.right = new Node("C");
+
+    tree.root.left.left = new Node("D");
+    tree.root.left.right = new Node("E");
+
+    tree.root.left.right.left = new Node("F");
+
+    //      A
+    //     / \
+    //    B   C
+    //   / \
+    //  D   E
+    //     /
+    //    F
+
+    const result = tree.inorderTraverse();
+
+    expect(JSON.stringify(result)).toBe(
+      JSON.stringify(["D", "B", "E", "F", "A", "C"])
+    );
+  });
 });
