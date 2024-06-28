aa4105959dab3c74f19a901990baefaa467bc4d8
hmy751 <hmy751@gmail.com>
2024-06-28 17:19:16 +0900

    feat: binary tree 및 ,preorderTraverse 메서드 구현

---
+++ src/dataStructure/binaryTree.ts
@@ -0,0 +1,44 @@
+type ValueType = string | number | Record<string, unknown> | Array<unknown>;
+
+export class Node {
+  value: ValueType;
+  left: Node | null;
+  right: Node | null;
+
+  constructor(value: ValueType) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BinaryTree {
+  root: Node | null;
+
+  constructor() {
+    this.root = null;
+  }
+
+  preorderTraverse(
+    current: Node | null = this.root,
+    result: Array<ValueType> = []
+  ): Array<ValueType> {
+    if (current) {
+      result.push(current.value);
+    }
+
+    if (current?.left) {
+      this.preorderTraverse(current.left, result);
+    }
+
+    if (current?.right) {
+      this.preorderTraverse(current.right, result);
+    }
+
+    return result;
+  }
+
+  inorderTraverse() {}
+
+  postorderTraverse() {}
+}
