0dacb5f59415d8f8c4093bd39dc7b0ebf0f41cfa
hmy751 <hmy751@gmail.com>
2024-07-03 17:26:35 +0900

    chore: binary search tree, add, printPreorder 메서드 주석 추가

--- src/dataStructure/binarySearchTree.ts
+++ src/dataStructure/binarySearchTree.ts
@@ -47,6 +47,17 @@ export class BinarySearchTree {
       return;
     }
   }
+  // 다음 데이터를 통해 호출할 때 주의할 점, linked list처럼 연결된 부분이 없으면
+  // 이전 노드와의 연결성을 잃기 때문에 유의 하기
+  // if (current?.value > value) {
+  //   if (current?.left) {
+  //     this.add(value, current?.left);
+  //     return;
+  //   }
+
+  //   current.left = newNode;
+  //   return;
+  // }
 
   remove() {}
 
@@ -70,4 +81,7 @@ export class BinarySearchTree {
 
     return result;
   }
+  // return을 통해 반환해야 하는 경우를 구분하자
+  // 위에 경우 this.printPreorder()을 호출하고 마지막 또는 기본 조건에서
+  // result를 return한다.
 }
