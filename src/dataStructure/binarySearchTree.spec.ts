bc5e548b3282c87e18c33eaf8ab3b53f47abdedf
hmy751 <hmy751@gmail.com>
2024-07-04 13:53:57 +0900

    test: binary search tree, 하위 노드가 없는 노드를 삭제할 때, 테스트 작성

--- src/dataStructure/binarySearchTree.spec.ts
+++ src/dataStructure/binarySearchTree.spec.ts
@@ -27,4 +27,41 @@ describe("Test Binary Search Tree", () => {
       JSON.stringify([7, 3, 2, 6, 5, 11, 8, 15])
     );
   });
+
+  it("하위 노드가 없는 노드를 삭제할 때", () => {
+    const tree = new BinarySearchTree();
+
+    tree.add(7);
+    tree.add(3);
+    tree.add(11);
+    tree.add(2);
+    tree.add(6);
+    tree.add(5);
+    tree.add(8);
+    tree.add(15);
+
+    //      7
+    //    /   \
+    //   3     11
+    //  / \   /  \
+    // 2   6  8  15
+    //    /
+    //   5
+
+    tree.remove(8);
+
+    //      7
+    //    /   \
+    //   3     11
+    //  / \      \
+    // 2   6      15
+    //    /
+    //   5
+
+    const result = tree.printPreorder();
+
+    expect(JSON.stringify(result)).toBe(
+      JSON.stringify([7, 3, 2, 6, 5, 11, 15])
+    );
+  });
 });
