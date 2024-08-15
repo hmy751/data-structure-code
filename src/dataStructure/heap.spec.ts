af0ea05b4bd99e71eade34a659d4381d5e0e8249
hmy751 <hmy751@gmail.com>
2024-08-15 21:30:50 +0900

    test: heap, 요소 삭제에 대한 테스트 작성

--- src/dataStructure/heap.spec.ts
+++ src/dataStructure/heap.spec.ts
@@ -18,5 +18,23 @@ describe("Test Heap", () => {
     );
   });
 
-  it("요소를 삭제하면, 힙 구조에 따라 요소들이 정렬된다.", () => {});
+  it("요소를 삭제하면, 힙 구조에 따라 요소들이 정렬된다.", () => {
+    const maxHeap = new MaxHeap();
+
+    maxHeap.add(4);
+    maxHeap.add(10);
+    maxHeap.add(49);
+    maxHeap.add(33);
+    maxHeap.add(24);
+    maxHeap.add(6);
+    maxHeap.add(1);
+    maxHeap.add(77);
+
+    expect(maxHeap.remove()).toBe(77);
+    expect(maxHeap.remove()).toBe(49);
+
+    expect(JSON.stringify(maxHeap.list)).toBe(
+      JSON.stringify(["none", 33, 24, 10, 4, 1, 6])
+    );
+  });
 });
