1ab28395e4e4c164cc1ab77e565f1da5daaa841d
hmy751 <hmy751@gmail.com>
2024-08-15 20:20:16 +0900

    feat: binary search tree, remove 메서드 구현

--- src/dataStructure/binarySearchTree.ts
+++ src/dataStructure/binarySearchTree.ts
@@ -85,10 +85,46 @@ export class BinarySearchTree {
     return result;
   }
 
+  /**
+   * remove는 재귀적으로 호출하여 타겟 노드를 찾아간다.
+   * 타겟을 찾기전에는 자식 노드에 removedNode를 할당하여 찾아 들어가고, current를 반환하여 부모와의 연결을 유지한다.
+   * 타겟을 찾고 나서는 케이스에 따라 나뉜다.
+   * 1. 자식 노드가 없을 때는 null을 반환함으로서 그 위치에서 자기 자신을 삭제한다.
+   * 2. 한쪽이 없는 경우는 자식 노드를 반환함으로서 자기 자신의 연결은 끊고 자식 노드를 상위로 연결시킨다.
+   * 3. 둘 다 있는 경우는 오른쪽 서브 트리의 가장 왼쪽 노드 즉 작은 노드를 찾는다. 이후 값을 자기 자신에게 할당하고 오른쪽 서브 트리 부터 재귀적 조회를 시작하고
+   * 해당 노드를 삭제한다.
+   */
   remove(value: ValueType): Node | null {
-    // const target = this.search(value);
-    // console.log(target);
-    // target[1].left = null;
+    const removeNode = (current: Node | null, value: ValueType) => {
+      if (!current) return null;
+
+      if (value < current.value) {
+        current.left = removeNode(current.left, value);
+        return current;
+      }
+
+      if (value > current.value) {
+        current.right = removeNode(current.right, value);
+        return current;
+      }
+
+      if (current.left === null && current.right === null) return null;
+
+      if (current.left === null) return current.right;
+      if (current.right === null) return current.left;
+
+      let min = current.right;
+      while (min.left !== null) {
+        min = min.left;
+      }
+
+      current.value = min.value;
+      current.right = removeNode(current.right, min.value);
+      return current;
+    };
+
+    this.root = removeNode(this.root, value);
+    return this.root;
   }
 
   printPreorder(
