f635bc43e08614e303d2fe2512d5bb6dc86e2c8b
hmy751 <hmy751@gmail.com>
2024-08-17 20:57:09 +0900

    test: graph, bfs 조회 테스트 작성

--- src/dataStructure/graph.spec.ts
+++ src/dataStructure/graph.spec.ts
@@ -19,5 +19,21 @@ describe("Test Graph", () => {
     expect(JSON.stringify(graph.dfs(1))).toBe(JSON.stringify([1, 2, 4, 5, 3]));
   });
 
-  it("엣지를 추가하면 그래프의 노드로 추가된다.", () => {});
+  it("bfs로 조회하면 가장 가까운 노드부터 조회한다.", () => {
+    const graph = new Graph();
+
+    graph.addNode(1);
+    graph.addNode(2);
+    graph.addNode(3);
+    graph.addNode(4);
+    graph.addNode(5);
+
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(2, 4);
+    graph.addEdge(3, 5);
+    graph.addEdge(4, 5);
+
+    expect(JSON.stringify(graph.bfs(1))).toBe(JSON.stringify([1, 2, 3, 4, 5]));
+  });
 });
