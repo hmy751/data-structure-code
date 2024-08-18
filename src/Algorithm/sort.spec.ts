1f63c2c4d48359631f14ed4fd44646fbe12eb5c1
hmy751 <hmy751@gmail.com>
2024-08-19 00:55:55 +0900

    refactor: sort 알고리즘 테스트 수정

--- src/Algorithm/sort.spec.ts
+++ src/Algorithm/sort.spec.ts
@@ -1,9 +1,11 @@
 import { bubbleSort } from "./bubbleSort";
+import { selectionSort } from "./selectionSort";
 
-describe("Test Bubble Sort", () => {
+describe("Test Sort", () => {
   it("주어진 배열을 정렬한다.", () => {
     const arr = [3, 2, 5, 10, 1];
 
     expect(bubbleSort(arr)).toStrictEqual([1, 2, 3, 5, 10]);
+    expect(selectionSort(arr)).toStrictEqual([1, 2, 3, 5, 10]);
   });
 });
