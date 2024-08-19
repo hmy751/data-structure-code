88c80588ed8f70fe3ead0195f3bfe8dc2f4f171f
hmy751 <hmy751@gmail.com>
2024-08-19 12:40:54 +0900

    fix: sort 테스트 케이스 초기화 오류 수정

--- src/Algorithm/Sort/sort.spec.ts
+++ src/Algorithm/Sort/sort.spec.ts
@@ -2,14 +2,36 @@ import { bubbleSort } from "./bubbleSort";
 import { selectionSort } from "./selectionSort";
 import { insertionSort } from "./insertionSort";
 import { mergeSort } from "./mergeSort";
+import { quickSort } from "./quickSort";
 
 describe("Test Sort", () => {
-  it("주어진 배열을 정렬한다.", () => {
+  it("버블 정렬 테스트", () => {
     const arr = [3, 2, 5, 10, 1];
 
     expect(bubbleSort(arr)).toStrictEqual([1, 2, 3, 5, 10]);
+  });
+
+  it("선택 정렬 테스트", () => {
+    const arr = [3, 2, 5, 10, 1];
+
     expect(selectionSort(arr)).toStrictEqual([1, 2, 3, 5, 10]);
+  });
+
+  it("삽입 정렬 테스트", () => {
+    const arr = [3, 2, 5, 10, 1];
+
     expect(insertionSort(arr)).toStrictEqual([1, 2, 3, 5, 10]);
+  });
+
+  it("병합 정렬 테스트", () => {
+    const arr = [3, 2, 5, 10, 1];
+
     expect(mergeSort(arr)).toStrictEqual([1, 2, 3, 5, 10]);
+  });
+
+  it("퀵 정렬 테스트", () => {
+    const arr = [3, 2, 5, 10, 1];
+
+    expect(quickSort(arr)).toStrictEqual([1, 2, 3, 5, 10]);
   });
 });
